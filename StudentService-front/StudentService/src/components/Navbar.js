5f8fb1a8845bfa109cf041e466fd370cbd05d3df
Sara Poparic <poparic.ra60.2018@uns.ac.rs>
2023-01-23 21:16:49 +0100

    fix: navbar

--- StudentService-front/StudentService/src/components/Navbar.js
+++ StudentService-front/StudentService/src/components/Navbar.js
@@ -4,16 +4,16 @@ import logo from '../assets/logo.png';
 
 function Navbar() {
 
-    const links = ["STUDENTS", "PROFESORS"]
+    const links = ["STUDENTS", "PROFESSORS"]
 
     return(
-        <div className='navbar-container'>
-            <div className='navbar-logo'>
-                <img className='navbar-logo-img' src={logo} alt="Logo"/>
+        <div className='flex w-full bg-stone-900 text-xl text-white'>
+            <div className='h-20 ml-5 mt-2'>
+                <img className='h-5/6 mt-1' src={logo} alt="Logo"/>
             </div>
-            <div className='navbar-links' style={{"marginLeft": "auto"}}>
+            <div className='flex ml-auto mt-8 mr-8' >
                 {links.map((link) => (
-                    <div className='navbar-link' key={link}>{link}</div>
+                    <div className='mr-16 cursor-pointer ' key={link}>{link}</div>
                 ))}
             </div>
         </div>
