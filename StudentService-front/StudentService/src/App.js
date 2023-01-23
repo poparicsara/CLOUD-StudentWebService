0e6e2a9bdf93cbd1a48a5c9ef8882db940476c5b
Sara Poparic <poparic.ra60.2018@uns.ac.rs>
2023-01-23 21:44:45 +0100

    feat: professors

--- StudentService-front/StudentService/src/App.js
+++ StudentService-front/StudentService/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Students from './pages/Students';
+import Professors from './pages/Professors';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-        <Route exact path="/" element={<Students/>} />
+          <Route exact path="/" element={<Students/>} />
+          <Route exact path="/professors" element={<Professors/>} />
         </Routes>
       </div>
     </Router>
