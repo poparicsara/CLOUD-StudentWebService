f4863d9863345874cb629197da9b61c9bf20952e
Sara Poparic <poparic.ra60.2018@uns.ac.rs>
2023-01-25 11:06:04 +0100

    fix: ftn app post request

--- FTNBackend/app/views.py
+++ FTNBackend/app/views.py
@@ -2,7 +2,9 @@ import json
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
 from app.models import Professor, Student
+import requests
 
 # Create your views here.
 def index(request):
@@ -16,8 +18,19 @@ def get_students(request):
 @csrf_exempt
 def new_student(request):
     data = json.loads(request.body)
-    Student.objects.create(name=data['name'], surname=data['surname'], jmbg=data['jmbg'], index=data['index'])
-    return HttpResponse(status=201)
+
+    student = Student(name=data['name'], surname=data['surname'], jmbg=data['jmbg'], index=data['index'])
+    student_json = model_to_dict(student)
+
+    headers = {'Content-type': 'application/json'}
+    response = requests.post('http://uns:8080/uns/students', data=json.dumps(student_json), headers=headers, verify=False) 
+
+    if response.status_code == 200:
+        Student.objects.create(name=data['name'], surname=data['surname'], jmbg=data['jmbg'], index=data['index'])
+        return HttpResponse(status=201)
+    else:
+        return HttpResponse(status=400)
+
 
 def get_professors(request):
     professors = Professor.objects.all().values()
--- PMFFrontend/StudentService/src/modals/NewProfessor.js
+++ PMFFrontend/StudentService/src/modals/NewProfessor.js
@@ -1,12 +1,52 @@
 import { useState } from "react";
 
-export default function NewProfessor({ modalIsOpen, setModalIsOpen, addProfessor }) {
+export default function NewProfessor({ modalIsOpen, setModalIsOpen }) {
 
     const [professor, setProfessor] = useState({
         name: "",
         surname: "",
         jmbg: "",
-    })
+    });
+    const [uploadedImage, setUploadedImage] = useState("");
+    const [base64, setBase64] = useState("");
+
+    const nameChangeHandler = (e) => {
+        const value = e.target.value;
+        setProfessor(() => {return {...professor, name: value}});
+        if (!value) {
+          return;
+        }
+    };
+
+    const surnameChangeHandler = (e) => {
+        const value = e.target.value;
+        setProfessor(() => {return {...professor, surname: value}});
+        if (!value) {
+          return;
+        }
+    };
+
+    const jmbgChangeHandler = (e) => {
+        const value = e.target.value;
+        setProfessor(() => {return {...professor, jmbg: value}});
+        if (!value) {
+          return;
+        }
+    };
+
+    const imageHandler = (e) => {
+        const reader = new FileReader();
+        reader.onload = () =>{
+            setUploadedImage(reader.result);
+            setBase64(reader.result);
+            console.log(reader.result);
+        }
+        reader.readAsDataURL(e.target.files[0])
+    };
+
+    const addProfessor = () => {
+        console.log(professor);
+    }
 
     return(
         <>
@@ -20,7 +60,7 @@ export default function NewProfessor({ modalIsOpen, setModalIsOpen, addProfessor
                                 {/*header*/}
                                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                                     <h3 className="text-3xl font-semibold text-stone-900">
-                                        New professor
+                                        New student
                                     </h3>
                                     <div className="ml-auto bg-transparent border-0 opacity-4 float-right text-3xl leading-none font-semibold cursor-pointer"
                                         onClick={() => setModalIsOpen(false)} >
@@ -30,7 +70,7 @@ export default function NewProfessor({ modalIsOpen, setModalIsOpen, addProfessor
 
                                 {/*body*/}
                                 <div className="w-96 text-sm">
-                                    <div className="w-96 md:flex items-center mb-6 mt-4 ml-6">
+                                    <div className="w-96 md:flex items-center mb-6 mt-10 ml-6">
                                         <div className="">
                                             <label
                                             className="block text-gray-500 font-bold md:text-right pr-4"
@@ -39,11 +79,12 @@ export default function NewProfessor({ modalIsOpen, setModalIsOpen, addProfessor
                                                 Name:
                                             </label>
                                         </div>
-                                        <div className="md:w-2/3 ml-auto mr-2">
+                                        <div className="md:w-2/3 ml-auto mr-5">
                                             <input
-                                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-56 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-900 text-xl"
+                                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-56 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-900 text-xl text-left"
                                             id="inline-full-name"
                                             type="text"
+                                            onChange={nameChangeHandler}
                                             />
                                         </div>
                                     </div>
@@ -56,11 +97,12 @@ export default function NewProfessor({ modalIsOpen, setModalIsOpen, addProfessor
                                                 Surname:
                                             </label>
                                         </div>
-                                        <div className="md:w-2/3 ml-auto mr-2">
+                                        <div className="md:w-2/3 ml-auto mr-5">
                                             <input
-                                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-56 py-2 px-4 text-gray-700 text-xl leading-tight focus:outline-none focus:bg-white focus:border-gray-900"
+                                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-56 py-2 px-4 text-gray-700 text-xl leading-tight focus:outline-none focus:bg-white focus:border-gray-900 text-left"
                                             id="inline-full-name"
                                             type="text"
+                                            onChange={surnameChangeHandler}
                                             />
                                         </div>
                                     </div>
@@ -73,12 +115,19 @@ export default function NewProfessor({ modalIsOpen, setModalIsOpen, addProfessor
                                                 JMBG:
                                             </label>
                                         </div>
-                                        <div className="md:w-2/3 ml-auto mr-2">
+                                        <div className="md:w-2/3 ml-auto mr-5">
                                             <input
-                                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-56 py-2 px-4 text-gray-700 text-xl leading-tight focus:outline-none focus:bg-white focus:border-gray-900"
+                                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-56 py-2 px-4 text-gray-700 text-xl leading-tight focus:outline-none focus:bg-white focus:border-gray-900 text-left"
                                             id="inline-full-name"
                                             type="text"
+                                            onChange={jmbgChangeHandler}
                                             />
+                                        </div>
+                                    </div>
+                                    <div className="w-11/12 md:flex items-center mb-6 ml-4 rounded-xl border-2 border-solid border-stone-300">
+                                        <div class="">
+                                            <input className="w-80 border-0" type="file" name="image" accept="image/png, image/jpeg" onChange={(e) => imageHandler(e)} />
+                                            <img className="ml-12 mb-8" alt='preview' src={uploadedImage} width="250" height="250"/>
                                         </div>
                                     </div>
                                 </div>
@@ -95,6 +144,7 @@ export default function NewProfessor({ modalIsOpen, setModalIsOpen, addProfessor
                                     <div
                                         className="bg-white text-stone-900 font-bold uppercase text-lg outline-none mr-1 mb-1 ease-linear cursor-pointer"
                                         type="button"
+                                        onClick={() => addProfessor()}
                                     >
                                         Save
                                     </div>
